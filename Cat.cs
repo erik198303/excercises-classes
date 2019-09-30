d5e81570d95efedeef6efdd4cf00d05fad58768c
erik suserud <eriksuserud@gmail.com>
2019-09-30 13:23:38 +0200

    Övning1-del1

---
+++ Cat.cs
@@ -0,0 +1,18 @@
+namespace excercises_classes
+{
+    class Cat
+    {   
+        public int age;
+        public string race;
+        public string name;
+        public void Bark()
+        {
+            System.Console.WriteLine("Inte idag");
+        }
+
+        public void Fetch()
+        {
+           System.Console.WriteLine("Nej tack!"); 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace excercises_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Cat myCat = new Cat();
+
+            myCat.name = "Olle";
+            myCat.race = "Sibirisk";
+            myCat.age = 12;
+
+            myCat.Fetch();
+            myCat.Bark();
+
+            System.Console.WriteLine("Katten heter {0}, är en {1} ras och är {2} gammal", myCat.name, myCat.race, myCat.age);
+
+        }
+    }
+}
+
+   
